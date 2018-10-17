833617dd7f85877a012a7e6f953495bd20bf5d1a
Justin Hartman <justin@hartman.me>
2018-10-18 01:03:06 +0200

    New paths file for using variables for common paths. Added requirements file to check minimum PHP requirements.

---
+++ config/paths.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Advanced PHP 7 eCommerce Website (https://22digital.agency)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @copyright Copyright (c) 22 Digital (https://22digital.agency)
+ * @copyright Copyright (c) Justin Hartman (https://justinhartman.blog)
+ * @author    Justin Hartman <justin@hartman.me> (https://justinhartman.blog)
+ * @link      https://github.com/justinhartman/complete-php7-ecom-website GitHub Project
+ * @since     0.1.0
+ * @license   https://opensource.org/licenses/AGPL-3.0 AGPL-3.0
+ */
+
+/**
+ * Use the DS to separate the directories in other defines.
+ */
+if (!defined('DS')) {
+    define('DS', DIRECTORY_SEPARATOR);
+}
+
+/**
+ * The full path to the directory WITHOUT a trailing DS.
+ */
+define('ROOT', dirname(__DIR__));
+
+/**
+ * Path to the config directory.
+ */
+define('CONFIG', ROOT . DS . 'config' . DS);
+
+/**
+ * Path to the vendor directory.
+ */
+define('VENDOR', ROOT . DS . 'vendor' . DS);
+
+/**
+ * Path to the includes directory.
+ */
+define('INC', ROOT . DS . 'inc' . DS);
---
+++ config/requirements.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Advanced PHP 7 eCommerce Website (https://22digital.agency)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @copyright Copyright (c) 22 Digital (https://22digital.agency)
+ * @copyright Copyright (c) Justin Hartman (https://justinhartman.blog)
+ * @author    Justin Hartman <justin@hartman.me> (https://justinhartman.blog)
+ * @link      https://github.com/justinhartman/complete-php7-ecom-website GitHub Project
+ * @since     0.1.0
+ * @license   https://opensource.org/licenses/AGPL-3.0 AGPL-3.0
+ */
+
+/**
+ * Check the version of PHP.
+ */
+if (version_compare(PHP_VERSION, '7.0.0') < 0) {
+    trigger_error('Your PHP version must be equal or higher than 7.0.0 to use this app.' . PHP_EOL, E_USER_ERROR);
+}
