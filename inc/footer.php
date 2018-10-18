ae21ffde8a8364a7500bb430c7c2411b84dba7a4
Justin Hartman <justin@hartman.me>
2018-10-18 02:00:15 +0200

    Moved js, css and font-awesome into new assets folder. Updated template files.

--- inc/footer.php
+++ inc/footer.php
@@ -14,30 +14,30 @@
         </div>
 
         <!-- Javascript -->
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/jquery.min.js"></script>
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/bootstrap.min.js"></script>
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/dialogFx.js"></script>
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/dialog-js.js"></script>
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/navigation/jquery.easing.js"></script>
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/flexslider/jquery.flexslider.js"></script>
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/navigation/scroll.js"></script>
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/navigation/jquery.sticky.js"></script>
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/owl-carousel/owl.carousel.min.js"></script>
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/isotope/isotope.pkgd.js"></script>
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/superfish/js/hoverIntent.js"></script>
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/superfish/js/superfish.js"></script>
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/tweecool.js"></script>
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/jquery.bpopup.js"></script>
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/pikaday/pikaday.js"></script>
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/classie.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/jquery.min.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/bootstrap.min.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/dialogFx.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/dialog-js.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/navigation/jquery.easing.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/flexslider/jquery.flexslider.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/navigation/scroll.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/navigation/jquery.sticky.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/owl-carousel/owl.carousel.min.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/isotope/isotope.pkgd.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/superfish/js/hoverIntent.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/superfish/js/superfish.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/tweecool.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/jquery.bpopup.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/pikaday/pikaday.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/classie.js"></script>
         <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/jquery.prettyphoto.js"></script>
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/script.js"></script>
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/booking.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/jquery.prettyphoto.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/script.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/booking.js"></script>
         <script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/gmap.js"></script>
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/gmap2.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/gmap.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/gmap2.js"></script>
     </body>
 </html>
--- inc/header.php
+++ inc/header.php
@@ -4,43 +4,36 @@
 <!--[if gt IE 9]><!-->	<html> <!--<![endif]-->
     <head>
         <meta charset="utf-8">
-        <title>Shop Home Page - PHP Ecommerce</title>
+        <title>PHP Ecommerce - Shop Home Page</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
         <meta name="keywords" content="">
         <meta name="author" content="">
-        <!-- Mobile Metas -->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- Favicon -->
-        <link rel="shortcut icon" href="<?php echo getenv('STORE_URL'); ?>/images/favicon.png">
-
-        <!-- CSS -->
-        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/css/bootstrap.css">
-        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/font-awesome/css/font-awesome.min.css">
-        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/js/isotope/isotope.css">
-        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/js/flexslider/flexslider.css">
-        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/js/owl-carousel/owl.carousel.css">
-        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/js/owl-carousel/owl.theme.css">
-        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/js/owl-carousel/owl.transitions.css">
-        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/js/superfish/css/megafish.css" media="screen">
-        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/js/superfish/css/superfish.css" media="screen">
-        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/js/pikaday/pikaday.css">
-        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/css/settings-panel.css">
-        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/css/style.css">
-        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/css/light.css">
-        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/css/responsive.css">
-
+        <link rel="shortcut icon" href="<?php echo getenv('STORE_URL'); ?>/assets/images/favicon.png">
+        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/assets/css/bootstrap.css">
+        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/assets/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/assets/js/isotope/isotope.css">
+        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/assets/js/flexslider/flexslider.css">
+        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/assets/js/owl-carousel/owl.carousel.css">
+        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/assets/js/owl-carousel/owl.theme.css">
+        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/assets/js/owl-carousel/owl.transitions.css">
+        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/assets/js/superfish/css/megafish.css" media="screen">
+        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/assets/js/superfish/css/superfish.css" media="screen">
+        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/assets/js/pikaday/pikaday.css">
+        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/assets/css/settings-panel.css">
+        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/assets/css/style.css">
+        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/assets/css/light.css">
+        <link rel="stylesheet" href="<?php echo getenv('STORE_URL'); ?>/assets/css/responsive.css">
         <!-- JS Font Script -->
         <script src="http://use.edgefonts.net/bebas-neue.js"></script>
-
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
-
         <!-- Modernizer -->
-        <script src="<?php echo getenv('STORE_URL'); ?>/js/modernizr.custom.js"></script>
+        <script src="<?php echo getenv('STORE_URL'); ?>/assets/js/modernizr.custom.js"></script>
     </head>
     <body class="multi-page">
         <div id="wrapper" class="wrapper">
@@ -49,7 +42,9 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-md-3 col-xs-5 logo">
-                            <a href="<?php echo getenv('STORE_URL'); ?>/index.php"><img src="https://via.placeholder.com/276x100" class="img-responsive" alt=""/></a>
+                            <a href="<?php echo getenv('STORE_URL'); ?>/index.php">
+                                <img src="https://via.placeholder.com/276x100" class="img-responsive" alt=""/>
+                            </a>
                         </div>
                         <div class="col-md-9 col-xs-7">
                             <div class="top-bar"></div>
